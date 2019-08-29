860eaf1d137e709cc1d31e1f8674db65078a1f55
Sergio Bernardi <sergiorbj93@gmail.com>
2019-08-28 22:59:23 -0300

    add exe page

---
+++ exercicio.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/exe.css">
+    <title>Curso PHP</title>
+</head>
+<body class="exercicio">
+
+    <header class="header">
+        <h1>Curso PHP</h1>
+        <h2>Visualização dos Exercicios</h2>
+    </header>
+    <nav class="navigation">
+      <a href='#' class=''>Sem formatação</a>
+      <a href='#' class=''>Voltar</a>
+    </nav>
+
+    <main class="main">
+        <div class="content">
+        <div>    
+    </main>
+
+    <footer class="footer">
+        SRBJ © <?= date('Y'); ?>
+    </footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Curso PHP</title>
+</head>
+<body>
+
+    <header class="header">
+        <h1>Curso PHP</h1>
+        <h2>Índice dos exercicios</h2>
+    </header>
+
+    <main class="main">
+        <div class="content">
+            <nav class="modulos">
+                <div class="modulo">
+                    <h3>Módulo 01</h3>
+                    <ul>
+                        <li><a href="exercicio.php">Exercicio A</a></li>
+                        <li><a href="#">Exercicio B</a></li>
+                        <li><a href="#">Exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 02</h3>
+                    <ul>
+                        <li><a href="#">Exercicio A</a></li>
+                        <li><a href="#">Exercicio B</a></li>
+                        <li><a href="#">Exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 03</h3>
+                    <ul>
+                        <li><a href="#">Exercicio A</a></li>
+                        <li><a href="#">Exercicio B</a></li>
+                        <li><a href="#">Exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 04</h3>
+                    <ul>
+                        <li><a href="#">Exercicio A</a></li>
+                        <li><a href="#">Exercicio B</a></li>
+                        <li><a href="#">Exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 05</h3>
+                    <ul>
+                        <li><a href="#">Exercicio A</a></li>
+                        <li><a href="#">Exercicio B</a></li>
+                        <li><a href="#">Exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 06</h3>
+                    <ul>
+                        <li><a href="#">Exercicio A</a></li>
+                        <li><a href="#">Exercicio B</a></li>
+                        <li><a href="#">Exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 07</h3>
+                    <ul>
+                        <li><a href="#">Exercicio A</a></li>
+                        <li><a href="#">Exercicio B</a></li>
+                        <li><a href="#">Exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 08</h3>
+                    <ul>
+                        <li><a href="#">Exercicio A</a></li>
+                        <li><a href="#">Exercicio B</a></li>
+                        <li><a href="#">Exercicio C</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 09</h3>
+                    <ul>
+                        <li><a href="#">Exercicio A</a></li>
+                        <li><a href="#">Exercicio B</a></li>
+                        <li><a href="#">Exercicio C</a></li>
+                    </ul>
+                </div>
+                        
+    </main>
+
+    <footer class="footer">
+        SRBJ © <?= date('Y'); ?>
+    </footer>
+
+</body>
+</html>
